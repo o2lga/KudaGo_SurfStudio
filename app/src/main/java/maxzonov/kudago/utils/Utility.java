3c8265cec2d121a33407e1986d820c98eedfc042
MaxZonov <max75758231@yandex.ru>
2018-05-15 23:17:01 +0300

    Fix data string building.

--- app/src/main/java/maxzonov/kudago/utils/Utility.java
+++ app/src/main/java/maxzonov/kudago/utils/Utility.java
@@ -3,6 +3,7 @@ package maxzonov.kudago.utils;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class Utility {
 
@@ -14,6 +15,10 @@ public class Utility {
         DateFormat formatDay = new SimpleDateFormat("dd");
         DateFormat formatMonth = new SimpleDateFormat("MMMM");
 
+        // TODO: different cities' timezones handling
+        formatDay.setTimeZone(TimeZone.getTimeZone("GMT+3"));
+        formatMonth.setTimeZone(TimeZone.getTimeZone("GMT+3"));
+
         String startDay = formatDay.format(dateStartFormatted);
         String startMonth = formatMonth.format(dateStartFormatted);
         String endDay = formatDay.format(dateEndFormatted);
@@ -21,12 +26,12 @@ public class Utility {
 
         String result;
 
-        if (startMonth.equals(endMonth)) {
-             result = startDay + " - " + endDay + " " + startMonth;
-        } else if (startDay.equals(endDay) && startMonth.equals(endMonth)) {
+        if (startDay.equals(endDay) && startMonth.equals(endMonth)) {
             result = startDay + " " + startMonth;
+        } else if (startMonth.equals(endMonth)) {
+            result = startDay + " - " + endDay + " " + startMonth;
         } else {
-            result = startDay + " " + startMonth + " - " + endDay + " " + startMonth;
+            result = startDay + " " + startMonth + " - " + endDay + " " + endMonth;
         }
 
         return result;
