4536bf87da438949d7f7d583a9d869c8e5a22d4a
MaxZonov <max75758231@yandex.ru>
2018-05-17 20:19:55 +0300

    Add changes to previous commit.

--- app/src/main/java/maxzonov/kudago/ui/main/MainActivity.java
+++ app/src/main/java/maxzonov/kudago/ui/main/MainActivity.java
@@ -1,5 +1,6 @@
 package maxzonov.kudago.ui.main;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -23,11 +24,14 @@ import io.reactivex.disposables.CompositeDisposable;
 import maxzonov.kudago.R;
 import maxzonov.kudago.model.main.Event;
 import maxzonov.kudago.ui.adapter.EventAdapter;
+import maxzonov.kudago.ui.details.DetailsActivity;
+import maxzonov.kudago.utils.OnEventClickListener;
 
 public class MainActivity extends MvpAppCompatActivity implements MainView {
 
     @InjectPresenter MainPresenter mainPresenter;
     private CompositeDisposable compositeDisposable;
+    private OnEventClickListener eventClickListener;
 
     @BindView(R.id.main_rv) RecyclerView recyclerView;
     @BindView(R.id.main_frame) FrameLayout frameLayout;
@@ -42,16 +46,17 @@ public class MainActivity extends MvpAppCompatActivity implements MainView {
         ButterKnife.bind(this);
 
         showProgress(true);
-        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                mainPresenter.getData(compositeDisposable);
-            }
-        });
+        swipeRefresh.setOnRefreshListener(() -> mainPresenter.getData(compositeDisposable));
 
         compositeDisposable = new CompositeDisposable();
 
         mainPresenter.getData(compositeDisposable);
+
+        eventClickListener = ((view, position) -> {
+            Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+            intent.putExtra("position", position);
+            startActivity(intent);
+        });
     }
 
     @Override
@@ -59,7 +64,7 @@ public class MainActivity extends MvpAppCompatActivity implements MainView {
         LinearLayoutManager layoutManagerCategory =
                 new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(layoutManagerCategory);
-        EventAdapter eventAdapter = new EventAdapter(events);
+        EventAdapter eventAdapter = new EventAdapter(events, eventClickListener);
         recyclerView.setAdapter(eventAdapter);
 
     }
