b6f37d3a6634754778bc7b24aa8e58a3b51fee1a
MaxZonov <max75758231@yandex.ru>
2018-05-18 22:38:29 +0300

    Fix activity details layout and class.

--- app/src/main/java/maxzonov/kudago/ui/details/DetailsActivity.java
+++ app/src/main/java/maxzonov/kudago/ui/details/DetailsActivity.java
@@ -1,15 +1,27 @@
 package maxzonov.kudago.ui.details;
 
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.widget.TextView;
 
+import com.arellomobile.mvp.MvpAppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import maxzonov.kudago.R;
 
-public class DetailsActivity extends AppCompatActivity {
+public class DetailsActivity extends MvpAppCompatActivity implements DetailsView {
+
+    @BindView(R.id.details_title) TextView tvTitle;
+    @BindView(R.id.details_subtitle) TextView tvSubTitle;
+    @BindView(R.id.details_full_descr) TextView tvFullDescr;
+    @BindView(R.id.details_tv_info_price) TextView tvPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        ButterKnife.bind(this);
     }
 }
