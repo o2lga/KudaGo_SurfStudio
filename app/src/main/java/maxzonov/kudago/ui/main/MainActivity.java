99a39213204b3d986372ab1d0804900eff667c6e
MaxZonov <max75758231@yandex.ru>
2018-05-19 16:04:40 +0300

    Add collapsing animation to toolbar.

--- app/src/main/java/maxzonov/kudago/ui/main/MainActivity.java
+++ app/src/main/java/maxzonov/kudago/ui/main/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -39,11 +40,15 @@ public class MainActivity extends MvpAppCompatActivity implements MainView {
     @BindView(R.id.main_tv_title) TextView tvTitle;
     @BindView(R.id.main_swipe_refresh) SwipeRefreshLayout swipeRefresh;
 
+    @BindView(R.id.main_toolbar) Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
 
         showProgress(true);
         swipeRefresh.setOnRefreshListener(() -> mainPresenter.getData(compositeDisposable));
